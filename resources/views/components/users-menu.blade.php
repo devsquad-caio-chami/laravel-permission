79687553f6f4949668928909604d4eab451d52d1
Maycon Medeiros <maycon.medeiros@devsquad.com>
2021-12-17 11:08:44 -0300

    change tab menu

--- resources/views/components/users-menu.blade.php
+++ resources/views/components/users-menu.blade.php
@@ -27,18 +27,18 @@
             </a>
 
             <a
-                href="#"
+                href="{{ route('roles-and-permissions.index') }}"
                 @class([
                     'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm',
-                    'border-indigo-500 text-indigo-600' => Route::is('roles.index'),
-                    'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' => !Route::is('roles.index')
+                    'border-indigo-500 text-indigo-600' => Route::is('roles-and-permissions.index'),
+                    'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' => !Route::is('roles-and-permissions.index')
                 ])
             >
                 <svg
                     @class([
                         '-ml-0.5 mr-2 h-5 w-5',
-                        'text-indigo-500' => Route::is('roles.index'),
-                        'text-gray-400 group-hover:text-gray-500' => !Route::is('roles.index')
+                        'text-indigo-500' => Route::is('roles-and-permissions.index'),
+                        'text-gray-400 group-hover:text-gray-500' => !Route::is('roles-and-permissions.index')
                     ])
                     class="text-indigo-500 "
                     xmlns="http://www.w3.org/2000/svg"
