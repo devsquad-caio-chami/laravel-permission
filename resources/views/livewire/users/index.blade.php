5a87353220b63086e1b022ee1a2f5244e38c0534
Maycon Medeiros <maycon.medeiros@devsquad.com>
2021-12-17 07:44:58 -0300

    extreme go horse users page

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -8,8 +8,92 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <livewire:users.data-table />
+            <x-users-menu/>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Users
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Brands (see Roles on hover)
+                                        </th>
+                                        <th scope="col" class="relative px-6 py-3">
+                                            <span class="sr-only">Edit</span>
+                                            <span class="sr-only">Remove</span>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                                <div class="relative rounded-lg flex items-center space-x-3">
+                                                    <div class="flex-shrink-0 mr-2">
+                                                        <img class="h-10 w-10 rounded-full" src="{{ $user->profile_photo_url }}" alt="">
+                                                    </div>
+                                                    <div class="flex-1 min-w-0">
+                                                        <p class="text-sm font-medium text-gray-900">
+                                                            {{ $user->name }}
+                                                        </p>
+                                                        <p class="text-sm text-gray-500 truncate">
+                                                            {{ $user->email }}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                @php
+                                                    $brands = DB::table('landlord.model_has_roles')
+                                                        ->selectRaw('tenants.name, group_concat(roles.name) as roles')
+                                                        ->join('landlord.tenants', 'team_id', '=', 'landlord.tenants.id')
+                                                        ->join('landlord.roles', 'role_id', '=', 'landlord.roles.id')
+                                                        ->where('model_id', '=', $user->id)
+                                                        ->groupBy('model_has_roles.team_id')
+                                                        ->get()
+                                                @endphp
+                                                @foreach($brands as $brand)
+                                                    <div class="inline-flex mr-2" x-data="{ tooltip: false }" x-on:mouseenter="tooltip = true" x-on:mouseleave="tooltip = false">
+                                                        <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800" >
+                                                            {{ ucfirst($brand->name) }}
+                                                        </span>
+                                                        <div x-show="tooltip" class="z-50 absolute bg-gray-50 border-graphite border-2 rounded p-2 mt-1">
+                                                            @foreach(explode(',', $brand->roles) as $role)
+                                                                <span class="inline-flex items-center px-3 py-1 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
+                                                                {{ $role }}
+                                                            </span>
+                                                            @endforeach
+
+                                                        </div>
+                                                    </div>
+                                                @endforeach
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <div class="flex divide-x-2">
+                                                    <a href="#"
+                                                       class="text-indigo-600 hover:text-indigo-900 p-2">Edit</a>
+                                                    <a href="#"
+                                                       class="text-indigo-600 hover:text-indigo-900 p-2">Remove</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-5">
+                {{ $users->links() }}
             </div>
         </div>
     </div>
