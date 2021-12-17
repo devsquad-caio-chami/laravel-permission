e7053d5e35a36fc571f760bccc6852faf40faa44
devsquad-caio-chami <caio.chami@devsquad.com>
2021-12-17 11:55:55 -0300

    fix: Fix sections component.

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,6 +1,7 @@
 <div>
     <select
         {{ $attributes->merge(['class' => 'mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md']) }}>
+        <option value="">Select an option</option>
         @foreach ($options as $value => $text)
             <option value="{{ $value }}">{{ $text }}</option>
         @endforeach
